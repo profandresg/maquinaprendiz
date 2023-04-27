a78e101c3bb1c46cdf51e3d2e6b98209fa886eb2
profandresg <66098671+profandresg@users.noreply.github.com>
2023-04-27 14:22:32 -0500

    Update minimglearn.py

--- minimglearn.py
+++ minimglearn.py
@@ -171,3 +171,29 @@ def make_forge(n_samples):
     mask[np.array([0, 1, 5, 26])] = 0 # Set four specific samples to False in the mask
     X, y = X[mask], y[mask] # Apply the mask to the dataset
     return X, y
+  
+def plot_2d_classification(classifier, X, fill=False, ax=None, eps=None,
+                           alpha=1, cm=cm3):
+    # multiclass
+    if eps is None:
+        eps = X.std() / 2.
+
+    if ax is None:
+        ax = plt.gca()
+
+    x_min, x_max = X[:, 0].min() - eps, X[:, 0].max() + eps
+    y_min, y_max = X[:, 1].min() - eps, X[:, 1].max() + eps
+    xx = np.linspace(x_min, x_max, 1000)
+    yy = np.linspace(y_min, y_max, 1000)
+
+    X1, X2 = np.meshgrid(xx, yy)
+    X_grid = np.c_[X1.ravel(), X2.ravel()]
+    decision_values = classifier.predict(X_grid)
+    ax.imshow(decision_values.reshape(X1.shape), extent=(x_min, x_max,
+                                                         y_min, y_max),
+              aspect='auto', origin='lower', alpha=alpha, cmap=cm)
+    ax.set_xlim(x_min, x_max)
+    ax.set_ylim(y_min, y_max)
+    ax.set_xticks(())
+    ax.set_yticks(())
+
