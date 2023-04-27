65dd5420189cb5ece90a9260520fda166e07d529
profandresg <66098671+profandresg@users.noreply.github.com>
2023-04-27 12:57:04 -0500

    Add files via upload

---
+++ minimglearn.py
@@ -0,0 +1,173 @@
+from matplotlib.colors import ListedColormap, colorConverter, LinearSegmentedColormap
+
+
+cm_cycle = ListedColormap(['#0000aa', '#ff5050', '#50ff50', '#9040a0', '#fff000'])
+cm3 = ListedColormap(['#0000aa', '#ff2020', '#50ff50'])
+cm2 = ListedColormap(['#0000aa', '#ff2020'])
+
+# create a smooth transition from the first to to the second color of cm3
+# similar to RdBu but with our red and blue, also not going through white,
+# which is really bad for greyscale
+
+cdict = {'red': [(0.0, 0.0, cm2(0)[0]),
+                 (1.0, cm2(1)[0], 1.0)],
+
+         'green': [(0.0, 0.0, cm2(0)[1]),
+                   (1.0, cm2(1)[1], 1.0)],
+
+         'blue': [(0.0, 0.0, cm2(0)[2]),
+                  (1.0, cm2(1)[2], 1.0)]}
+
+ReBl = LinearSegmentedColormap("ReBl", cdict)
+
+
+import numpy as np
+from sklearn.datasets import make_blobs
+
+# Define a function to create a wave dataset
+def make_wave(n_samples=1000):
+    rnd = np.random.RandomState(42) # Create a random state generator with a fixed seed
+    x = rnd.uniform(-3, 3, size=n_samples) # Generate uniformly distributed data between -3 and 3
+    y_no_noise = (np.sin(4 * x) + x) # Generate y values based on a sine wave with some noise added
+    y = (y_no_noise + rnd.normal(size=len(x))) / 2 # Add normally distributed noise to the y values
+    return x.reshape(-1, 1), y
+
+
+def discrete_scatter(x1, x2, y=None, markers=None, s=10, ax=None,
+                     labels=None, padding=.2, alpha=1, c=None, markeredgewidth=None):
+    """Adaption of matplotlib.pyplot.scatter to plot classes or clusters.
+    Parameters
+    ----------
+    x1 : nd-array
+        input data, first axis
+    x2 : nd-array
+        input data, second axis
+    y : nd-array
+        input data, discrete labels
+    cmap : colormap
+        Colormap to use.
+    markers : list of string
+        List of markers to use, or None (which defaults to 'o').
+    s : int or float
+        Size of the marker
+    padding : float
+        Fraction of the dataset range to use for padding the axes.
+    alpha : float
+        Alpha value for all points.
+    """
+    import numpy as np
+    import matplotlib as mpl
+    import matplotlib.pyplot as plt
+    from matplotlib.colors import ListedColormap, colorConverter, LinearSegmentedColormap
+    if ax is None:
+        ax = plt.gca()
+
+    if y is None:
+        y = np.zeros(len(x1))
+
+    unique_y = np.unique(y)
+
+    if markers is None:
+        markers = ['o', '^', 'v', 'D', 's', '*', 'p', 'h', 'H', '8', '<', '>'] * 10
+
+    if len(markers) == 1:
+        markers = markers * len(unique_y)
+
+    if labels is None:
+        labels = unique_y
+
+    # lines in the matplotlib sense, not actual lines
+    lines = []
+
+    current_cycler = mpl.rcParams['axes.prop_cycle']
+
+    for i, (yy, cycle) in enumerate(zip(unique_y, current_cycler())):
+        mask = y == yy
+        # if c is none, use color cycle
+        if c is None:
+            color = cycle['color']
+        elif len(c) > 1:
+            color = c[i]
+        else:
+            color = c
+        # use light edge for dark markers
+        if np.mean(colorConverter.to_rgb(color)) < .4:
+            markeredgecolor = "grey"
+        else:
+            markeredgecolor = "black"
+
+        lines.append(ax.plot(x1[mask], x2[mask], markers[i], markersize=s,
+                             label=labels[i], alpha=alpha, c=color,
+                             markeredgewidth=markeredgewidth,
+                             markeredgecolor=markeredgecolor)[0])
+
+    if padding != 0:
+        pad1 = x1.std() * padding
+        pad2 = x2.std() * padding
+        xlim = ax.get_xlim()
+        ylim = ax.get_ylim()
+        ax.set_xlim(min(x1.min() - pad1, xlim[0]), max(x1.max() + pad1, xlim[1]))
+        ax.set_ylim(min(x2.min() - pad2, ylim[0]), max(x2.max() + pad2, ylim[1]))
+
+    return lines
+
+def plot_2d_separator(classifier, X, fill=False, ax=None, eps=None, alpha=1,
+                      cm=cm2, linewidth=None, threshold=None,
+                      linestyle="solid"):
+    #print('hola')
+    import numpy as np
+    import matplotlib.pyplot as plt
+    # binary?
+    if eps is None:
+        eps = X.std() / 2.
+
+    if ax is None:
+        ax = plt.gca()
+
+    x_min, x_max = X[:, 0].min() - eps, X[:, 0].max() + eps
+    y_min, y_max = X[:, 1].min() - eps, X[:, 1].max() + eps
+    xx = np.linspace(x_min, x_max, 1000)
+    yy = np.linspace(y_min, y_max, 1000)
+
+    X1, X2 = np.meshgrid(xx, yy)
+    X_grid = np.c_[X1.ravel(), X2.ravel()]
+    try:
+        #print('decision values')
+        decision_values = classifier.decision_function(X_grid)
+        levels = [0] if threshold is None else [threshold]
+        fill_levels = [decision_values.min()] + levels + [
+            decision_values.max()]
+    except AttributeError:
+        # no decision_function
+        print('')
+        decision_values = classifier.predict_proba(X_grid)[:, 1]
+        levels = [.5] if threshold is None else [threshold]
+        fill_levels = [0] + levels + [1]
+    if fill:
+        #print('fill')
+        ax.contourf(X1, X2, decision_values.reshape(X1.shape),
+                    levels=fill_levels, alpha=alpha, cmap=cm)
+    else:
+        #print('no fill')
+        ax.contour(X1, X2, decision_values.reshape(X1.shape), levels=levels,
+                   colors="black", alpha=alpha, linewidths=linewidth,
+                   linestyles=linestyle, zorder=5)
+
+    ax.set_xlim(x_min, x_max)
+    ax.set_ylim(y_min, y_max)
+    ax.set_xticks(())
+    ax.set_yticks(())
+
+
+import numpy as np
+from sklearn.datasets import make_blobs
+
+# Define a function to create a custom dataset called "forge"
+def make_forge(n_samples):
+    # Generate a dataset with two blobs, centered at different points, with n_samples samples total
+    X, y = make_blobs(centers=2, random_state=4, n_samples=n_samples)
+    y[np.array([7, 27])] = 0 # Set two specific samples to have a label of 0
+    mask = np.ones(len(X), dtype=bool) # Create a boolean mask with all values initially set to True
+    mask[np.array([0, 1, 5, 26])] = 0 # Set four specific samples to False in the mask
+    X, y = X[mask], y[mask] # Apply the mask to the dataset
+    return X, y
