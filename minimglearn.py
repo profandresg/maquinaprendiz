912cc61f48be2c4322225253cb64e866a3b96841
profandresg <66098671+profandresg@users.noreply.github.com>
2023-05-03 17:03:08 -0500

    Update minimglearn.py

--- minimglearn.py
+++ minimglearn.py
@@ -206,3 +206,32 @@ def plot_2d_classification(classifier, X, fill=False, ax=None, eps=None,
     ax.set_xticks(())
     ax.set_yticks(())
 
+    
+#-------------------------------------------    
+def plot_tree_partition(X, y, tree, ax=None):
+    if ax is None:
+        ax = plt.gca()
+    eps = X.std() / 2.
+
+    x_min, x_max = X[:, 0].min() - eps, X[:, 0].max() + eps
+    y_min, y_max = X[:, 1].min() - eps, X[:, 1].max() + eps
+    xx = np.linspace(x_min, x_max, 1000)
+    yy = np.linspace(y_min, y_max, 1000)
+
+    X1, X2 = np.meshgrid(xx, yy)
+    X_grid = np.c_[X1.ravel(), X2.ravel()]
+
+    Z = tree.predict(X_grid)
+    Z = Z.reshape(X1.shape)
+    faces = tree.apply(X_grid)
+    faces = faces.reshape(X1.shape)
+    border = ndimage.laplace(faces) != 0
+    ax.contourf(X1, X2, Z, alpha=.4, cmap=cm2, levels=[0, .5, 1])
+    ax.scatter(X1[border], X2[border], marker='.', s=1)
+
+    discrete_scatter(X[:, 0], X[:, 1], y, ax=ax)
+    ax.set_xlim(x_min, x_max)
+    ax.set_ylim(y_min, y_max)
+    ax.set_xticks(())
+    ax.set_yticks(())
+    return ax
